40320bd25c1ef933298b03d9f9bed605c3f8312c
NIMCOO <nimco77001@gmail.com>
2023-12-07 07:35:22 +0000

    Done Exercise

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Good Luck 💪🏾
+ function removeDuplicates(arr){
+return [...new Set(arr)];
+ }
+  const numbers = [1, 0, 1, 0] 
+  console.log(removeDuplicates(numbers));
+
+//removeDuplicates(["New", "York", "City"])
+function removeDuplicates(arr){
+    return [...new Set(arr)];
+     }
+      const towns = ["New", "York", "City"] 
+  console.log(removeDuplicates(towns))   
+
+//(["Hassan", "Aisha", "Aisha"])
+function removeDuplicates(arr){
+    return [...new Set(arr)];
+     }
+      const names= ["Hassan", "Aisha", "Aisha"] 
+  console.log(removeDuplicates(names))
